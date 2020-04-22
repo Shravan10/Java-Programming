dac382f6edc77758dfe9ca26f4d5354a6cadc6e6
Shravan10 <shravankumarghanchi@gmail.com>
2020-04-23 04:13:03 +0530

    First commit static method in java

---
+++ StaticMethod/src/com/Launcher.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    StaticMethodandFeilds.main(args);
+            System.out.println(StaticMethodandFeilds.myNumber);
+            AnotherMain.main(args);
+            
+	}
+
+}
